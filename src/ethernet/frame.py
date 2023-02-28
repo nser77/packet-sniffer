7fb2b00edc0fe3ed07559beeb82fbee2767828e2
nser77 <104022475+nser77@users.noreply.github.com>
2023-03-01 00:07:14 +0100

    Update frame.py

--- src/ethernet/frame.py
+++ src/ethernet/frame.py
@@ -2,19 +2,24 @@ from binascii import hexlify
 from bitstring import BitStream
 
 class EthernetFrame(object):
+    header_size=0
+    footer_size=0
+
     def __init__(self, bitstream):
         self.setDstMac(    bitstream[:48])
         self.setSrcMac(    bitstream[48:96])
         self.setEtherType( bitstream[96:112])
-        #self.setFcs(      bitstream[:-32])
+        self.setFcs(       bitstream[:-32])
 
     def setDstMac(self, bitstream):
         if isinstance(bitstream, BitStream):
             self.dst_mac=hexlify(bitstream.bytes, ":")
+            self.header_size += 48
 
     def setSrcMac(self, bitstream):
         if isinstance(bitstream, BitStream):
             self.src_mac=hexlify(bitstream.bytes, ":")
+            self.header_size += 48
 
     def setEtherType(self, bitstream):
         if isinstance(bitstream, BitStream):
@@ -25,3 +30,8 @@ class EthernetFrame(object):
                     self.ether_type='arp'
                 case '086dd':
                     self.ether_type='ipv6'
+
+            self.header_size += 16
+
+    def setFcs(self, bitstream):
+       self.footer_size += 32
