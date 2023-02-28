49dc77a3314614874e386c9a869c07842478063f
nser77 <104022475+nser77@users.noreply.github.com>
2023-02-28 18:43:25 +0100

    Create sniffer.py

---
+++ src/sniffer.py
@@ -0,0 +1,33 @@
+from socket import socket
+from socket import PF_PACKET, SOCK_RAW, ntohs
+
+from bitstring import BitStream
+
+from ethernet.frame import EthernetFrame
+from internet.ipv4.ipv4_packet import Ipv4Packet
+
+class Sniffer:
+    def start(self):
+        conn = socket(PF_PACKET, SOCK_RAW, ntohs(3))
+
+        while True:
+            raw, addr = conn.recvfrom(65536)
+
+            bitstream=BitStream(raw)
+            ethernet_frame = EthernetFrame(bitstream)
+            ipv4_packet=Ipv4Packet(bitstream)
+            
+            layers=[]
+            layers.append(ethernet_frame)
+            layers.append(ipv4_packet)
+
+            yield layers
+
+try:
+    sniffer=Sniffer()
+    
+    for packet in sniffer.start():
+        if len(packet) >= 2:
+            print(packet[1].version)
+except KeyboardInterrupt:
+    print("\n"
