090d9a7e55f5ab35abbecf189a95cc4444f1d2d2
nser77 <104022475+nser77@users.noreply.github.com>
2023-03-01 00:07:46 +0100

    Update frame.py

--- src/ethernet/frame.py
+++ src/ethernet/frame.py
@@ -1,37 +1,102 @@
-from binascii import hexlify
 from bitstring import BitStream
 
-class EthernetFrame(object):
+class Ipv4Packet(object):
     header_size=0
-    footer_size=0
+    header_options=False
+    header_options_size=288
 
     def __init__(self, bitstream):
-        self.setDstMac(    bitstream[:48])
-        self.setSrcMac(    bitstream[48:96])
-        self.setEtherType( bitstream[96:112])
-        self.setFcs(       bitstream[:-32])
+        self.setVersion(        bitstream[:4])
+        self.setIhl(            bitstream[4:8])
+        self.setTos(            bitstream[8:16])
+        self.setTotalLength(    bitstream[16:32])
+        self.setIdentification( bitstream[32:48])
+        self.setFlag0(          bitstream[48:49])
+        self.setFlag1(          bitstream[49:50])
+        self.setFlag2(          bitstream[50:51])
+        self.setFragmentOffset( bitstream[51:64])
+        self.setTtl(            bitstream[64:72])
+        self.setProtocol(       bitstream[72:80])
+        self.setHeaderChecksum( bitstream[80:96])
+        self.setSrc(            bitstream[96:128])
+        self.setDst(            bitstream[128:160])
 
-    def setDstMac(self, bitstream):
+        if self.ihl > 5 and self.ihl <= 15:
+             self.header_options=True
+             self.setOptions(bitstream[160:self.header_options_size])
+
+    def setVersion(self, bitstream):
         if isinstance(bitstream, BitStream):
-            self.dst_mac=hexlify(bitstream.bytes, ":")
-            self.header_size += 48
+            self.version=bitstream.int
+            self.header_size += 4
 
-    def setSrcMac(self, bitstream):
+    def setIhl(self, bitstream):
         if isinstance(bitstream, BitStream):
-            self.src_mac=hexlify(bitstream.bytes, ":")
-            self.header_size += 48
+            self.ihl=bitstream.int
+            self.header_size += 4
 
-    def setEtherType(self, bitstream):
+    def setTos(self, bitstream):
+        # not implemented yet
         if isinstance(bitstream, BitStream):
-            match bitstream.hex:
-                case '0800':
-                    self.ether_type='ipv4'
-                case '0806':
-                    self.ether_type='arp'
-                case '086dd':
-                    self.ether_type='ipv6'
+            self.tos=bitstream.int
+            self.header_size += 8
 
+    def setTotalLength(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.total_length=bitstream.uint
             self.header_size += 16
 
-    def setFcs(self, bitstream):
-       self.footer_size += 32
+    def setIdentification(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.identification=bitstream.uint
+            self.header_size += 16
+
+    def setFlag0(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.flag0=bitstream.int
+            self.header_size += 1
+
+    def setFlag1(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.flag1=bitstream.int
+            self.header_size += 1
+
+    def setFlag2(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.flag2=bitstream.int
+            self.header_size += 1
+
+    def setFragmentOffset(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.fragment_offset=bitstream.uint
+            self.header_size += 13
+
+    def setTtl(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.ttl=bitstream.int
+            self.header_size += 8
+
+    def setProtocol(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.protocol=bitstream.int
+            self.header_size += 8
+
+    def setHeaderChecksum(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.header_checksum=bitstream.uint
+            self.header_size += 16
+
+    def setSrc(self, bitstream):
+        if isinstance(bitstream, BitStream):
+           self.src=".".join(map(str, bitstream.bytes))
+           self.header_size += 32
+
+    def setDst(self, bitstream):
+        if isinstance(bitstream, BitStream):
+           self.dst=".".join(map(str, bitstream.bytes))
+           self.header_size += 32
+
+    def setOptions(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.options=bitstream.uint
+            self.header_size += self.header_options_size
