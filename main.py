062f0b88440629de89e9fdd3de51e1a3dd0cadb0
nser77 <104022475+nser77@users.noreply.github.com>
2023-02-28 18:55:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ sniffer=Sniffer()
     
 for packet in sniffer.start():
     if len(packet) >= 2:
+        # gettig 'version' ip packet field
         print(packet[1].version)
+        print(packet[0].src_mac)
