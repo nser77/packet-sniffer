447c0e61d67a41bdac812e0cf6c2200078e10af5
nser77 <104022475+nser77@users.noreply.github.com>
2023-03-01 00:15:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ sniffer=Sniffer()
     
 for packet in sniffer.start():
     if len(packet) >= 2:
+        # getting 'src_mac' ethernet frame field
+        print(packet[0].src_mac)
+        
         # gettig 'version' ip packet field
         print(packet[1].version)
-        print(packet[0].src_mac)
+
