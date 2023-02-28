6f519115d20c844723a9b845fbf4debf2cdb5753
nser77 <104022475+nser77@users.noreply.github.com>
2023-02-28 14:55:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,10 @@ class Sniffer:
         
         ethernet_frame = EthernetFrame(dst_mac, src_mac, ether_type)
 
-        return ethernet_frame, ether_type
+        return ethernet_frame
 
     def sliceIpv4Packet(self, raw):
-        ipv4_packet = Ipv4Packet(raw)
-        next = raw[ipv4_packet.size:]
-
-        return ipv4_packet, next
+        pass
 
     def start(self):
         conn = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
@@ -31,8 +28,28 @@ class Sniffer:
             
             ip_version=bitstream[112:116].int
             ip_ihl=bitstream[116:120].int
+            ip_tos=bitstream[120:128].int
+            #ip_dscp=bitstream[123:128].int
+            ip_total_length=bitstream[128:144].uint
+            ip_id=bitstream[144:160].uint
+            ip_flag_0=bitstream[160:161].int
+            ip_flag_1=bitstream[161:162].int
+            ip_flag_2=bitstream[162:163].int
+            ip_fragment_offset=bitstream[163:176].uint
+            ip_ttl=bitstream[176:184].int
+            ip_protocol=bitstream[184:192].int
+            ip_checksum=bitstream[192:208].uint
+            ip_src=bitstream[208:240].bytes
+            ip_dst=bitstream[240:272].bytes
+            h_ip_src=".".join(map(str, ip_src))
+            h_ip_dst=".".join(map(str, ip_dst))
 
-            print(dst_mac, src_mac, ether_type, ip_version, ip_ihl)
+            if ethernet_frame.ether_type == 'ipv4':
+                print(ip_version, ip_ihl, ip_tos, ip_total_length,
+                      ip_id, ip_flag_0, ip_flag_1, ip_flag_2, ip_fragment_offset,
+                      ip_ttl, ip_protocol, ip_checksum,
+                      h_ip_src, h_ip_dst)
+
 
     def output(self, layers):
         pass
