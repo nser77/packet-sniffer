360b7b546eecb86ae173e2a4ccd04b7cad395eab
nser77 <104022475+nser77@users.noreply.github.com>
2023-03-08 19:16:04 +0100

    Create udp.py

---
+++ src/layers/transport/udp.py
@@ -0,0 +1,27 @@
+"""
+    https://datatracker.ietf.org/doc/html/rfc768
+"""
+
+from bitstring import BitStream
+
+class Udp(object):
+    header_size=0
+    header_options=False
+    header_options_size=288
+
+    def __init__(self, bitstream):
+        self.setSrcPort(              bitstream[:16])
+        self.setDstPort(              bitstream[16:32])
+
+    def setSrcPort(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.src_port=bitstream.uint
+            self.header_size += 16
+
+    def setDstPort(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.dst_port=bitstream.uint
+            self.header_size += 16
+
+    def next(self, bitstream):
+        pass
