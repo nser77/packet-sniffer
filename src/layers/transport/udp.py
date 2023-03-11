4a7570afaa44708c39b6414ba94ef684b38ed825
nser77 <104022475+nser77@users.noreply.github.com>
2023-03-11 18:24:48 +0100

    Update udp.py

--- src/layers/transport/udp.py
+++ src/layers/transport/udp.py
@@ -8,12 +8,12 @@ from layers.application.dns import Dns
 
 class Udp(object):
     header_size=0
-    header_options=False
-    header_options_size=288
 
     def __init__(self, bitstream):
         self.setSrcPort(              bitstream[:16])
         self.setDstPort(              bitstream[16:32])
+        self.setLength(               bitstream[32:48])
+        self.setChecksum(             bitstream[48:64])
 
     def setSrcPort(self, bitstream):
         if isinstance(bitstream, BitStream):
@@ -25,5 +25,17 @@ class Udp(object):
             self.dst_port=bitstream.uint
             self.header_size += 16
 
+    def setLength(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.length=bitstream.uint
+            self.header_size += 16
+
+    def setChecksum(self, bitstream):
+        if isinstance(bitstream, BitStream):
+            self.checksum=bitstream.bin
+            self.header_size += 16
+
     def next(self, bitstream):
+        if self.src_port == 53 or self.dst_port == 53:
+            return Dns(bitstream)
         return False
